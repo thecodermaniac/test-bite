854da184b28c4bddd7908bbf9872ccbf23dc42b8
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2024-07-05 16:33:52 +0530

    changed the endpoint

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("./../controller/userController.js");
 
-router.post("/lol", userController.identifyUser);
+router.post("/", userController.identifyUser);
 
 module.exports = router;
